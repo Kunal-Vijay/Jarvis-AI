2c8df0f6d559658d5dda448d3ecc93f3a9549b34
Kunal <kunal20vijay00@gmail.com>
2024-01-11 01:47:23 +0530

    jarvis AI

---
+++ App.js
@@ -0,0 +1,9 @@
+import React, {useEffect} from 'react';
+import AppNavigation from './src/navigation';
+import {apiCall} from './src/api/openAI';
+export default function App() {
+  useEffect(() => {
+    // apiCall('create an image or a dog playing with cat');
+  }, []);
+  return <AppNavigation />;
+}
---
+++ src/constants/index.js
@@ -0,0 +1,23 @@
+export const apiKey = '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';
+// in some cases your api key maybe already expired
+// try to use a new account to create an api key
+// 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
+export const dummyMessages = [
+  {
+    role: 'user',
+    content: 'How are you?',
+  },
+  {
+    role: 'assistant',
+    content: "I'm fine, How may i help you today.",
+  },
+  {
+    role: 'user',
+    content: 'create an image of a dog playing with cat',
+  },
+  {
+    role: 'assistant',
+    content:
+      'https://storage.googleapis.com/pai-images/ae74b3002bfe4b538493ca7aedb6a300.jpeg',
+  },
+];
